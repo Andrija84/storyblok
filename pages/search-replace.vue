c73e1cda08371f3c4e309a98cd1011d3d40d4512
andrija84 <andrijanikolic1984@gmail.com>
2021-05-24 15:33:53 +0200

    In progress

--- pages/search-replace.vue
+++ pages/search-replace.vue
@@ -2,13 +2,13 @@
   <section>
     <h2 class="">This is a search and replace page</h2>
     <p>Here you can search and replace inside all stories on Storyblok</p>
-    <input type="text" placeholder="Enter word you want to replace">
-    <input type="text" placeholder="Enter word to replace word entered above">
+    <input type="text" placeholder="Enter word you want to replace" v-model="findWord">
+    <input type="text" placeholder="Enter word to replace word entered above" v-model="replaceWord">
     <p v-if="showSuccessMessage">Word has been replaced</p>
     <p v-if="showErrorMessage">No matched words</p>
-    <button @click="replaceWord">Replace</button>
+    <button @click="searchAndReplace">Replace</button>
 
-
+    
     <pre>{{posts}}</pre>
 
 
@@ -20,35 +20,108 @@ export default {
   data () {
     return {
       posts:[],
+      findWord: '',
+      replaceWord: '',
       showSuccessMessage: false,
       showErrorMessage: false
     }
   },
   methods:{
-    getAllPosts(){
+
+    getAllStories(){
       this.$storyapi.get('cdn/stories', {
         starts_with: 'posts/',
         version: 'draft'
       }).then((res) => {
         console.log(res.data.stories)
-        this.posts = res.data.stories
+
+
+
       }).catch((res) => {
         if (!res.response) {
           console.error(res)
-          context.error({ statusCode: 404, message: 'Failed to receive content form api' })
         } else {
           console.error(res.response.data)
-          context.error({ statusCode: res.response.status, message: res.response.data })
         }
       })
     },
-     replaceWord(){
-       console.log('clicked')
-     }
+
+    searchAndReplace(){
+      this.$storyapi.get('cdn/stories', {
+        starts_with: 'posts/',
+        version: 'draft'
+      }).then((res) => {
+        console.log(res.data.stories)
+
+        this.posts = res.data.stories;
+        var findWord = this.findWord;
+        var replaceWord = this.replaceWord;
+
+       /* var originalMsg = JSON.stringify(res.data.stories);
+        
+        var regexp = new RegExp(findWord, "gi");
+        var updatedMsg = originalMsg.replace(regexp, replaceWord);
+        var newObj = JSON.parse(updatedMsg); 
+        console.log(newObj);
+       */ 
+
+
+       res.data.stories.forEach(post => {
+          // post is a single object
+          var originalPost= JSON.stringify(post);
+          var regexp = new RegExp(findWord, "gi");
+          var replacedPost = originalPost.replace(regexp, replaceWord);
+          let updatedPost = JSON.parse(replacedPost); 
+
+          //console.log(updatedPost)
+
+                var id = updatedPost.id;
+                var slug = updatedPost.slug;
+                var name = updatedPost.name;
+                var content = updatedPost.content.content
+
+          //https://www.storyblok.com/docs/api/management#core-resources/stories/update-story
+
+                this.$storyapi.put('cdn/stories/' + id , {
+
+                  "story": {
+                    "name": name,
+                    "slug": slug,
+                    "id": id,
+                    "content": {
+                      "component": "post",
+                      "content": content
+                    }
+                  },
+                  "force_update": 1,
+                  //"publish": 1
+
+                }).then(response => {
+                  console.log(response)
+                }).catch(error => { 
+                  console.log(error)
+                })
+         
+
+       })
+
+
+
+
+
+      }).catch((res) => {
+        if (!res.response) {
+          console.error(res)
+        } else {
+          console.error(res.response.data)
+        }
+      })
+    },
+
   },
  
   created(){     
-    this.getAllPosts();
+    this.getAllStories();
   }
 
 }
